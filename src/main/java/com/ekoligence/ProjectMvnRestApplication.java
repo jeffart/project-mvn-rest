5ec6ba1db79de8bd25ac7db42ee697f1b82c47d8
jeffart <dekogha@gmail.com>
2021-09-19 13:28:53 +0200

    First commit for this project

---
+++ src/main/java/com/ekoligence/ProjectMvnRestApplication.java
@@ -0,0 +1,13 @@
+package com.ekoligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMvnRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMvnRestApplication.class, args);
+	}
+
+}
